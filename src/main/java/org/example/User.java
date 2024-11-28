4b6e8c506f9b1ca825bf28be7a20cffb31685d5f
vHc05 <145705639+vHc05@users.noreply.github.com>
2024-11-28 20:55:18 +0000

    Add files via upload

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "aedreversemapping")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
